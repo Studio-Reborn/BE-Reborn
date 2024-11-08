ae0a1d48889e722308eaad34c5cac0872b2e1ffd
youmin29 <babybana0829@naver.com>
2024-11-08 15:15:24 +0900

    ✨ Feat: 리본 리메이크 제품 요청 추가

--- src/modules/remake/remake.controller.ts
+++ src/modules/remake/remake.controller.ts
@@ -6,24 +6,36 @@ Author : 이유민
 History
 Date        Author      Status      Description
 2024.11.03  이유민      Created     
-2024.11.03  이유민      Modified    리본 리메이크 제품 요청 기능 추가
-2024.11.06  이유민      Modified    리본 리메이크 제품 추천 기능 추가
+2024.11.03  이유민      Modified    리본 리메이크 제품 추천 기능 추가
+2024.11.06  이유민      Modified    리본 리메이크 제품 요청 기능 추가
 */
-import { Body, Controller, Post, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Query,
+  BadRequestException,
+} from '@nestjs/common';
 import { RemakeService } from 'src/modules/remake/remake.service';
-import { Remake } from 'src/modules/remake/remake.entity';
+import { RemakeDTO } from 'src/modules/remake/remake.dto';
 
 @Controller('remake')
 export class RemakeController {
   constructor(private readonly remakeService: RemakeService) {}
 
-  @Post()
-  async create(@Body() remakeData: Partial<Remake>): Promise<Remake> {
-    return this.remakeService.createRemake(remakeData);
+  @Post('/request')
+  async create(@Body() remakeDTO: RemakeDTO) {
+    const { remake_product } = remakeDTO;
+
+    if (!remake_product)
+      throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return this.remakeService.createRemake({ user_id: 1, remake_product });
   }
 
   @Get()
-  async recommendProduct(@Query('thing') thing: string): Promise<object> {
+  async recommendProduct(@Query('thing') thing: string) {
     return this.remakeService.recommendRemake(thing);
   }
 }
--- src/modules/remake/remake.dto.ts
+++ src/modules/remake/remake.dto.ts
@@ -7,16 +7,12 @@ History
 Date        Author      Status      Description
 2024.11.03  이유민      Created     
 2024.11.03  이유민      Modified    리본 리메이크 제품 요청 기능 추가
+2024.11.07  이유민      Modified    제품 요청 DTO 수정
 */
-import { IsInt, IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RemakeDTO {
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsInt()
-  user_id: number;
-
   @ApiProperty()
   @IsNotEmpty()
   @IsString()

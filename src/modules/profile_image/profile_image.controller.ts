ba26b23f3f9bb3a320a2ce5927b5ef12bf148d68
youmin29 <babybana0829@naver.com>
2024-11-18 19:43:12 +0900

    📚 Docs: swagger 문서 작성

--- src/modules/profile_image/profile_image.controller.ts
+++ src/modules/profile_image/profile_image.controller.ts
@@ -7,6 +7,7 @@ History
 Date        Author      Status      Description
 2024.11.13  이유민      Created     
 2024.11.13  이유민      Modified    프로필 이미지 추가
+2024.11.18  이유민      Modified    swagger 추가
 */
 import {
   Controller,
@@ -18,8 +19,10 @@ import {
 import { ProfileService } from 'src/modules/profile_image/profile_image.service';
 import { UsersService } from 'src/modules/users/users.service';
 import { JwtAuthGuard } from 'src/modules/auth/jwt/jwt-auth.guard';
+import { ApiTags, ApiOperation, ApiHeader } from '@nestjs/swagger';
 
 @Controller('profile')
+@ApiTags('프로필 이미지 API')
 export class ProfileController {
   constructor(
     private readonly profileService: ProfileService,
@@ -29,6 +32,15 @@ export class ProfileController {
   // 사용자 프로필 이미지 조회
   @Get()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '프로필 이미지 조회 API',
+    description: '사용자 본인의 프로필 이미지를 조회한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
   async findProfile(@Req() req) {
     if (!req.user.user_id)
       throw new UnauthorizedException('로그인 후 이용 가능합니다.');
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,24 +8,46 @@ Date        Author      Status      Description
 2024.11.13  이유민      Created     
 2024.11.13  이유민      Modified    사용자 정보 조회 추가
 2024.11.13  이유민      Modified    닉네임 수정 추가
+2024.11.18  이유민      Modified    swagger 추가
 */
 import { Controller, Get, Patch, Req, Body, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt/jwt-auth.guard';
 import { UsersService } from 'src/modules/users/users.service';
+import { ApiTags, ApiOperation, ApiHeader } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('유저 API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   // 사용자 정보 조회
   @Get()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '본인 정보 조회 API',
+    description: '사용자 본인의 정보를 조회한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
   async findUserById(@Req() req) {
     return await this.usersService.findUserById(req.user.user_id);
   }
 
+  // 닉네임 수정
   @Patch('/nickname')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '닉네임 수정 API',
+    description: '사용자의 닉네임을 수정한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
   async updateNickname(@Req() req, @Body('nickname') nickname: string) {
     return await this.usersService.updateNickname(req.user.user_id, nickname);
   }

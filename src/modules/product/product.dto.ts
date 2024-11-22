17cca76bcf21913b7fd3dbb64dd65880a9ff878a
youmin29 <babybana0829@naver.com>
2024-11-22 18:58:57 +0900

    ✨ Feat: market_id 추가

--- src/modules/product/product.dto.ts
+++ src/modules/product/product.dto.ts
@@ -9,6 +9,7 @@ Date        Author      Status      Description
 2024.11.07  이유민      Modified    상품 등록 기능 추가
 2024.11.08  이유민      Modified    리본 리메이크 제품 분리
 2024.11.20  이유민      Modified    상품 이미지 추가
+2024.11.21  이유민      Modified    market_id 추가
 */
 import { IsInt, IsString, IsNotEmpty, IsIn } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
@@ -39,4 +40,8 @@ export class ProductDTO {
   @IsNotEmpty()
   @IsInt()
   product_image_id: number;
+
+  @ApiProperty()
+  @IsInt()
+  market_id: number;
 }

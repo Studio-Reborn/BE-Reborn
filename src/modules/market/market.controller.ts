07141735b78f7935a92c364099c96f66c06dfc84
youmin29 <babybana0829@naver.com>
2024-11-22 18:59:41 +0900

    ✨ Feat: 에코마켓 기능 추가

--- src/modules/market/market.controller.ts
+++ src/modules/market/market.controller.ts
@@ -8,7 +8,17 @@ Date        Author      Status      Description
 2024.11.21  이유민      Created     
 2024.11.21  이유민      Modified    에코마켓 추가
 */
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Body,
+  Param,
+  Req,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt/jwt-auth.guard';
 import { ApiTags, ApiOperation, ApiHeader } from '@nestjs/swagger';
 import { MarketDTO } from 'src/modules/market/market.dto';
@@ -23,15 +33,81 @@ export class MarketController {
   @Post()
   @UseGuards(JwtAuthGuard)
   @ApiOperation({
-    summary: '본인 정보 조회 API',
-    description: '사용자 본인의 정보를 조회한다.',
+    summary: '에코마켓 생성 API',
+    description: '에코마켓을 생성한다.',
   })
   @ApiHeader({
     name: 'Authorization',
     description: 'Bearer 토큰 형식의 JWT',
     required: true,
   })
-  async createMarket(@Body() marketDTO: MarketDTO) {
-    return await this.marketService.createMarket(marketDTO);
+  async createMarket(@Req() req, @Body() marketDTO: MarketDTO) {
+    const { profile_image_id, market_name, market_detail } = marketDTO;
+    return await this.marketService.createMarket({
+      user_id: req.user.user_id,
+      profile_image_id,
+      market_name,
+      market_detail,
+    });
+  }
+
+  // 에코마켓 전체 조회
+  @Get()
+  @ApiOperation({
+    summary: '에코마켓 전체 조회 API',
+    description: '에코마켓을 전체 조회한다.',
+  })
+  async findMarketAll() {
+    return await this.marketService.findMarketAll();
+  }
+
+  // 에코마켓 개별 조회
+  @Get(':id')
+  @ApiOperation({
+    summary: '에코마켓 개별 조회 API',
+    description: '에코마켓을 개별로 조회한다.',
+  })
+  async findMarketById(@Param('id', ParseIntPipe) id: number) {
+    return await this.marketService.findMarketById(id);
+  }
+
+  // 에코마켓 정보 수정
+  @Patch('/info/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '에코마켓 정보 수정 API',
+    description: '에코마켓 정보를 수정한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async updateMarketInfo(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: MarketDTO,
+  ) {
+    return await this.marketService.updateMarketInfo(
+      req.user.user_id,
+      id,
+      updateData,
+    );
+  }
+
+  // 에코마켓 삭제 요청
+  @Patch('/request/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '에코마켓 삭제 요청 API',
+    description: '관리자에게 에코마켓 삭제 요청한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async deleteRequestMarket(@Req() req, @Param('id', ParseIntPipe) id: number) {
+    return await this.marketService.deleteRequestMarket(req.user.user_id, id);
   }
 }
--- src/modules/market/market.entity.ts
+++ src/modules/market/market.entity.ts
@@ -7,6 +7,8 @@ History
 Date        Author      Status      Description
 2024.11.21  이유민      Created     
 2024.11.21  이유민      Modified    에코마켓 추가
+2024.11.21  이유민      Modified    user_id 추가
+2024.11.22  이유민      Modified    is_deletion_requested 추가
 */
 import {
   Entity,
@@ -22,6 +24,9 @@ export class Market {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ nullable: false })
+  user_id: number;
+
   @Column({ default: 1 })
   profile_image_id: number;
 
@@ -33,6 +38,9 @@ export class Market {
 
   @Column({ default: false })
   is_verified: boolean;
+
+  @Column({ default: false })
+  is_deletion_requested: boolean;
 
   @CreateDateColumn()
   created_at: Date;

d7d9424eac83cb968be5d7bd1bc53c8d6737da55
youmin29 <babybana0829@naver.com>
2024-12-04 21:14:32 +0900

    ✨ Feat: 관리자 기능 추가

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -10,6 +10,8 @@ Date        Author      Status      Description
 2024.11.13  이유민      Modified    닉네임 수정 추가
 2024.11.18  이유민      Modified    swagger 추가
 2024.11.19  이유민      Modified    id로 사용자 정보 조회 추가
+2024.12.04  이유민      Modified    전체 사용자 조회 추가
+2024.12.04  이유민      Modified    사용자 유형 수정 추가
 */
 import {
   Controller,
@@ -20,6 +22,7 @@ import {
   Body,
   UseGuards,
   ParseIntPipe,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt/jwt-auth.guard';
 import { UsersService } from 'src/modules/users/users.service';
@@ -30,8 +33,18 @@ import { ApiTags, ApiOperation, ApiHeader } from '@nestjs/swagger';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // 본인 정보 조회
+  // 전체 사용자 정보 조회
   @Get()
+  @ApiOperation({
+    summary: '본인 정보 조회 API',
+    description: '사용자 본인의 정보를 조회한다.',
+  })
+  async findAllUser() {
+    return await this.usersService.findAllUser();
+  }
+
+  // 본인 정보 조회
+  @Get('/my')
   @UseGuards(JwtAuthGuard)
   @ApiOperation({
     summary: '본인 정보 조회 API',
@@ -47,7 +60,7 @@ export class UsersController {
   }
 
   // 사용자 정보 조회
-  @Get(':id')
+  @Get('/info/:id')
   @ApiOperation({
     summary: '사용자 정보 조회 API',
     description: 'user_id를 이용해 사용자 정보를 조회한다.',
@@ -70,5 +83,24 @@ export class UsersController {
   })
   async updateNickname(@Req() req, @Body('nickname') nickname: string) {
     return await this.usersService.updateNickname(req.user.user_id, nickname);
+  }
+
+  // 사용자 유형 수정
+  @Patch('/role/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '닉네임 수정 API',
+    description: '사용자의 닉네임을 수정한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async updateRole(@Req() req, @Param('id', ParseIntPipe) id: number) {
+    if (req.user.role !== 'admin')
+      throw new UnauthorizedException('관리자만 접근 가능합니다.');
+
+    return await this.usersService.updateRole(id);
   }
 }

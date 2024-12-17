c3707a440b1999153ead332483b93f2084e0abf2
youmin29 <babybana0829@naver.com>
2024-12-17 12:54:07 +0900

    ✨ Feat: 마켓 좋아요 기능 추가

--- src/modules/like/like.controller.ts
+++ src/modules/like/like.controller.ts
@@ -8,6 +8,7 @@ Date        Author      Status      Description
 2024.12.16  이유민      Created     
 2024.12.16  이유민      Modified    상품 좋아요 추가
 2024.12.17  이유민      Modified    product_id 타입 수정
+2024.12.17  이유민      Modified    마켓 좋아요 추가
 */
 import {
   Controller,
@@ -18,11 +19,12 @@ import {
   UnauthorizedException,
   Param,
   Req,
+  ParseIntPipe,
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt/jwt-auth.guard';
 import { ApiTags, ApiOperation, ApiHeader } from '@nestjs/swagger';
-import { ProductLikeDTO } from 'src/modules/like/like.dto';
+import { ProductLikeDTO, MarketLikeDTO } from 'src/modules/like/like.dto';
 import { LikeService } from 'src/modules/like/like.service';
 
 @Controller('like')
@@ -58,20 +60,11 @@ export class LikeController {
 
   // 상품의 총 좋아요 조회
   @Get('/product/all/:id')
-  @UseGuards(JwtAuthGuard)
   @ApiOperation({
-    summary: '상품 좋아요 조회 API',
-    description: '상품 좋아요 데이터를 조회한다.',
+    summary: '상품 총 좋아요 조회 API',
+    description: '상품 전체 좋아요 데이터를 조회한다.',
   })
-  @ApiHeader({
-    name: 'Authorization',
-    description: 'Bearer 토큰 형식의 JWT',
-    required: true,
-  })
-  async findProductLikeByProductId(@Req() req, @Param('id') id: string) {
-    if (!req.user.user_id)
-      throw new UnauthorizedException('로그인 후 이용 가능합니다.');
-
+  async findProductLikeByProductId(@Param('id') id: string) {
     if (!id) throw new BadRequestException('입력하지 않은 값이 있습니다.');
 
     return await this.likeService.findProductLikeByProductId(id);
@@ -81,8 +74,8 @@ export class LikeController {
   @Get('/product/user/:id')
   @UseGuards(JwtAuthGuard)
   @ApiOperation({
-    summary: '상품 좋아요 조회 API',
-    description: '상품 좋아요 데이터를 조회한다.',
+    summary: '사용자의 제품 좋아요 조회 API',
+    description: '사용자가 해당 제품에 좋아요를 눌렀는지 조회한다.',
   })
   @ApiHeader({
     name: 'Authorization',
@@ -99,6 +92,71 @@ export class LikeController {
     if (!id) throw new BadRequestException('입력하지 않은 값이 있습니다.');
 
     return await this.likeService.findProductLikeByProductIdAndUserId(
+      req.user.user_id,
+      id,
+    );
+  }
+
+  // 마켓 좋아요 생성
+  @Post('/market')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '마켓 좋아요 생성 API',
+    description: '마켓 좋아요 데이터를 생성한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async createMarketLike(@Req() req, @Body() marketLikeDTO: MarketLikeDTO) {
+    const { market_id } = marketLikeDTO;
+    if (!req.user.user_id)
+      throw new UnauthorizedException('로그인 후 이용 가능합니다.');
+
+    if (!market_id)
+      throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return await this.likeService.createMarketLike({
+      user_id: req.user.user_id,
+      market_id,
+    });
+  }
+
+  // 마켓의 총 좋아요 조회
+  @Get('/market/all/:id')
+  @ApiOperation({
+    summary: '마켓 총 좋아요 조회 API',
+    description: '마켓 전체 좋아요 데이터를 조회한다.',
+  })
+  async findMarketLikeByMarketId(@Param('id', ParseIntPipe) id: number) {
+    if (!id) throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return await this.likeService.findMarketLikeByMarketId(id);
+  }
+
+  // 사용자가 해당 마켓 좋아요 눌렀는지 조회
+  @Get('/market/user/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '사용자의 마켓 좋아요 조회 API',
+    description: '사용자가 해당 마켓에 좋아요를 눌렀는지 조회한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async findMarketLikeByMarketIdAndUserId(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    if (!req.user.user_id)
+      throw new UnauthorizedException('로그인 후 이용 가능합니다.');
+
+    if (!id) throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return await this.likeService.findMarketLikeByMarketIdAndUserId(
       req.user.user_id,
       id,
     );
--- src/modules/like/like.module.ts
+++ src/modules/like/like.module.ts
@@ -7,17 +7,20 @@ History
 Date        Author      Status      Description
 2024.12.16  이유민      Created     
 2024.12.16  이유민      Modified    상품 좋아요 추가
+2024.12.17  이유민      Modified    마켓 좋아요 추가
 */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductLike } from 'src/modules/like/entity/product_like.entity';
 import { ProductLikeRepository } from 'src/modules/like/repository/product_like.repository';
+import { MarketLike } from 'src/modules/like/entity/market_like.entity';
+import { MarketLikeRepository } from 'src/modules/like/repository/market_like.repository';
 import { LikeService } from 'src/modules/like/like.service';
 import { LikeController } from 'src/modules/like/like.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductLike])],
-  providers: [ProductLikeRepository, LikeService],
+  imports: [TypeOrmModule.forFeature([ProductLike, MarketLike])],
+  providers: [ProductLikeRepository, MarketLikeRepository, LikeService],
   controllers: [LikeController],
 })
 export class LikeModule {}

0f1fb75d6c417709b528247635485a07eab7a1db
youmin29 <babybana0829@naver.com>
2025-01-15 22:51:12 +0900

    🔨 Refactor: 코드 리팩토링

--- src/modules/cart/cart.dto.ts
+++ src/modules/cart/cart.dto.ts
@@ -7,18 +7,17 @@ History
 Date        Author      Status      Description
 2025.01.11  이유민      Created     
 2025.01.11  이유민      Modified    장바구니 추가
+2025.01.15  이유민      Modified    코드 리팩토링
 */
-import { IsNotEmpty, IsInt } from 'class-validator';
+import { IsInt } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CartDTO {
   @ApiProperty()
-  @IsNotEmpty()
   @IsInt()
   product_id: string;
 
   @ApiProperty()
-  @IsNotEmpty()
   @IsInt()
   quantity: number;
 }

a7ff902a9518db5a9da4c9adf9d4f3eb31a2ecf3
youmin29 <babybana0829@naver.com>
2025-01-18 18:01:41 +0900

    ✨ Feat: 리본 리메이크 판매 내역 추가

--- src/modules/billing/repository/order_items.repository.ts
+++ src/modules/billing/repository/order_items.repository.ts
@@ -9,6 +9,7 @@ Date        Author      Status      Description
 2024.11.26  이유민      Modified    주문 제품 추가
 2024.12.30  이유민      Modified    리본 리메이크 제품 판매 횟수 조회 추가
 2025.01.18  이유민      Modified    내 마켓 관련 API 추가
+2025.01.18  이유민      Modified    리본 리메이크 판매 내역 추가
 */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
@@ -38,6 +39,31 @@ export class OrderItemsRepository {
     if (!item) throw new NotFoundException('리소스를 찾을 수 없습니다.');
 
     return item;
+  }
+
+  async findRemakeItem(): Promise<OrderItems[]> {
+    return await this.orderItemsRepository
+      .createQueryBuilder('items')
+      .leftJoin('remake_product', 'product', 'product.id = items.product_id')
+      .leftJoin('orders', 'orders', 'items.order_id = orders.id')
+      .select([
+        'product.id AS product_id',
+        'product.name AS product_name',
+        'items.id AS items_id',
+        'items.price AS items_price',
+        'items.quantity AS items_quantity',
+        'orders.id AS orders_id',
+        'orders.postcode AS orders_postcode',
+        'orders.address AS orders_address',
+        'orders.detail_address AS orders_detail_address',
+        'orders.extra_address AS orders_extra_address',
+        'orders.name AS user_name',
+        'orders.phone AS user_phone',
+        'items.status AS items_status',
+        'items.tracking_number AS items_tracking_number',
+      ])
+      .where('items.category = "reborn"')
+      .getRawMany();
   }
 
   // 리본 리메이크 제품 판매 횟수 조회

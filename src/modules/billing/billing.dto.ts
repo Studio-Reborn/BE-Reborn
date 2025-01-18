efe9bcbe6325c558875be04ee9f81615a6e6d7a3
youmin29 <babybana0829@naver.com>
2025-01-18 17:02:59 +0900

    🔨 Refactor: 데이터 컬럼 추가

--- src/modules/billing/billing.dto.ts
+++ src/modules/billing/billing.dto.ts
@@ -12,6 +12,8 @@ Date        Author      Status      Description
 2024.11.28  이유민      Modified    category 추가
 2024.12.17  이유민      Modified    product_id 타입 수정
 2025.01.17  이유민      Modified    코드 리팩토링
+2025.01.18  이유민      Modified    name 및 phone 추가
+2025.01.18  이유민      Modified    tracking_number 및 status 추가
 */
 import { IsInt, IsString, IsNotEmpty, IsArray } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
@@ -35,6 +37,16 @@ export class OrderItemDTO {
   @ApiProperty()
   @IsString()
   category: string;
+}
+
+export class ItemUpdateDTO {
+  @ApiProperty()
+  @IsInt()
+  status: string;
+
+  @ApiProperty()
+  @IsString()
+  tracking_number: string;
 }
 
 export class TossPaymentDTO {
@@ -72,6 +84,16 @@ export class TossPaymentDTO {
   @ApiProperty()
   @IsString()
   extra_address: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
 
   @ApiProperty()
   @IsArray()

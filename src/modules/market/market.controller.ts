63cdb3767c6cbccd7dfc9852a5cd8f64080b6676
youmin29 <babybana0829@naver.com>
2025-01-21 00:43:47 +0900

    ✨ Feat: 요청 반려 관련 API 추가

--- src/modules/market/market.controller.ts
+++ src/modules/market/market.controller.ts
@@ -12,6 +12,7 @@ Date        Author      Status      Description
 2024.12.04  이유민      Modified    swagger 수정
 2025.01.02  이유민      Modified    검색 및 정렬 추가
 2025.01.18  이유민      Modified    내 마켓 관련 API 추가
+2025.01.20  이유민      Modified    요청 반려 관련 API 추가
 */
 import {
   Controller,
@@ -26,6 +27,7 @@ import {
   UseGuards,
   ParseIntPipe,
   UnauthorizedException,
+  BadRequestException,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt/jwt-auth.guard';
 import { ApiTags, ApiOperation, ApiHeader } from '@nestjs/swagger';
@@ -163,11 +165,50 @@ export class MarketController {
     description: 'Bearer 토큰 형식의 JWT',
     required: true,
   })
-  async checkCreateMarket(@Req() req, @Param('id', ParseIntPipe) id: number) {
+  async updateIsVerified(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body('is_verified') is_verified: string,
+  ) {
     if (req.user.role !== 'admin')
       throw new UnauthorizedException('관리자만 접근 가능합니다.');
 
-    return await this.marketService.checkCreateMarket(id);
+    if (
+      !is_verified ||
+      !['pending', 'approved', 'rejected'].includes(is_verified)
+    )
+      throw new BadRequestException('잘못된 값을 입력했습니다.');
+
+    return await this.marketService.updateIsVerified(id, is_verified);
+  }
+
+  // 에코마켓 재심사 요청
+  @Patch('/retry/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '에코마켓 정보 수정 API',
+    description: '에코마켓 정보를 수정한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async retryMarket(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: MarketDTO,
+  ) {
+    if (!req.user)
+      throw new UnauthorizedException('로그인 후 접근 가능합니다.');
+
+    const { profile_image_id, market_name, market_detail } = updateData;
+    return await this.marketService.retryMarket(id, {
+      profile_image_id,
+      market_name,
+      market_detail,
+      is_verified: 'pending',
+    });
   }
 
   // 에코마켓 정보 수정
@@ -225,10 +266,85 @@ export class MarketController {
   async deleteRemakeProductById(
     @Req() req,
     @Param('id', ParseIntPipe) id: number,
+    @Query('is_deletion_requested') is_deletion_requested: string,
   ) {
     if (req.user.role !== 'admin')
       throw new UnauthorizedException('관리자만 접근 가능합니다.');
 
-    return this.marketService.deleteMarketById(id);
+    if (
+      !is_deletion_requested ||
+      !['pending', 'approved', 'rejected'].includes(is_deletion_requested)
+    )
+      throw new BadRequestException('잘못된 값을 입력했습니다.');
+
+    return this.marketService.deleteMarketById(id, is_deletion_requested);
+  }
+
+  @Post('/rejection/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '삭제 요청된 에코마켓 삭제 API',
+    description: '관리자가 삭제 요청된 에코마켓을 확인 후 삭제한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async writeRejectionReason(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body('reason') reason: string,
+  ) {
+    if (!req.user)
+      throw new UnauthorizedException('로그인 후 사용 가능합니다.');
+
+    if (req.user.role !== 'admin')
+      throw new UnauthorizedException('관리자만 접근 가능합니다.');
+
+    if (!reason || !id)
+      throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return await this.marketService.writeRejectionReason(id, reason);
+  }
+
+  @Get('/rejection/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '삭제 요청된 에코마켓 삭제 API',
+    description: '관리자가 삭제 요청된 에코마켓을 확인 후 삭제한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async readRejectionReason(@Req() req, @Param('id', ParseIntPipe) id: number) {
+    if (!req.user)
+      throw new UnauthorizedException('로그인 후 사용 가능합니다.');
+
+    if (!id) throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return await this.marketService.readRejectionReason(id);
+  }
+
+  @Patch('/rejection/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '삭제 요청된 에코마켓 삭제 API',
+    description: '관리자가 삭제 요청된 에코마켓을 확인 후 삭제한다.',
+  })
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Bearer 토큰 형식의 JWT',
+    required: true,
+  })
+  async changeVisibility(@Req() req, @Param('id', ParseIntPipe) id: number) {
+    if (!req.user)
+      throw new UnauthorizedException('로그인 후 사용 가능합니다.');
+
+    if (!id) throw new BadRequestException('입력하지 않은 값이 있습니다.');
+
+    return await this.marketService.changeVisibility(id);
   }
 }
